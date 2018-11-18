a5051eb0aa450bdf0a9e8a1d21875c95aa93f525
Kons <kons0@libero.it>
2018-11-19 00:38:38 +0100

    [VS2017] Replacing broken reference

--- src/LanguageModel/LanguageModel/SyntaxNode.cs
+++ src/LanguageModel/LanguageModel/SyntaxNode.cs
@@ -1,5 +1,4 @@
-﻿using ImmutableObjectGraph;
-using ImmutableObjectGraph.CodeGeneration;
+using ImmutableObjectGraph.Generation;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
